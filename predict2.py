796caf919ad323e5cec3cdd1219a96e0935e7816
johncaged <573697439@qq.com>
2022-12-27 03:50:45 -0500

    FEA: change generation strategy

--- predict2.py
+++ predict2.py
@@ -23,16 +23,19 @@ def main():
     
     tokenizer = Tokenizer()
     # build and load model
-    model = BaseQuestioner(tokenizer, MultiStageAdapter(), auto_regressive=True)
+    model = BaseQuestioner(tokenizer, MultiStageAdapter(), auto_regressive=True, use_img_region_embedding=True)
+    # model = BaseQuestioner(tokenizer, MultiStageAdapter(), auto_regressive=True, use_img_region_embedding=False)
     model = ModelWrapper(model)
-    checkpoint = torch.load('./log/vg_224_512/checkpoint/checkpoint_39.pth', map_location='cpu')
-    model.load_state_dict(checkpoint['model'])
+    checkpoint = torch.load('./log/vg_224_512_embedding_new/checkpoint/best.pth', map_location='cpu')
+    # checkpoint = torch.load('./log/vg_224_512/checkpoint/best.pth', map_location='cpu')
+    model.load_state_dict(checkpoint['model'], strict=False)
     model.eval()
     del checkpoint
     caption_model: TextSampler = ToCuda(TopKSampler(model.module, k=3))
     model: TextSampler = ToCuda(TwoStageSampler(model.module, base_k=10, question_answer_sep_token=tokenizer.question_answer_sep_token, answer_type_mask_token=tokenizer.answer_type_mask_token))
     
     batch_size = 256 * dist.get_world_size()
+    # batch_size = 2
     
     cc3m = build_cc3m_dataset(tokenizer)
     gen_imgs = []
@@ -65,16 +68,16 @@ def main():
     print('generated data length at rank {}: {}'.format(dist.get_rank(), len(data['data'])))
     
     types = [
-        *[{'answer_type': 'what'} for _ in range(1)],
-        *[{'answer_type': 'how'} for _ in range(1)],
-        *[{'answer_type': 'where'} for _ in range(1)],
+        *[{'answer_type': 'what'} for _ in range(3)],
+        *[{'answer_type': 'how'} for _ in range(2)],
+        *[{'answer_type': 'where'} for _ in range(2)],
         *[{'answer_type': 'who'} for _ in range(1)],
         *[{'answer_type': 'why'} for _ in range(1)],
         *[{'answer_type': 'when'} for _ in range(1)]
     ]
     
     repeat_sample = len(types)
-    object_sample = 3
+    object_sample = 16
     
     # max_iter = 100000 // batch_size
     
@@ -95,16 +98,28 @@ def main():
         
         for k in range(object_sample):
             objects = batch[0]['objects']
-            sampled_objects = [random.choice(_objects) for _objects in objects]
+            object_embeddings = batch[0]['object_embeddings']
+            if 'video_input' in batch[0]:
+                del batch[0]['video_input']
+            
+            def sample(_object_items):
+                _object_items = list(zip(*_object_items))
+                return _object_items[min(k, len(_object_items) - 1)]
+
+            sampled_objects = [sample(_objects) for _objects in zip(objects, object_embeddings)]
+            # set region embedding
+            sampled_object_embeddings = torch.stack([_object[1] for _object in sampled_objects])
+            batch[0]['region'] = sampled_object_embeddings
+            batch[0]['caption_region'] = sampled_object_embeddings
             # generate region captions
-            processed_sampled_objects = [process_tip(_object) for _object in sampled_objects]
+            processed_sampled_objects = [process_tip(_object[0]) for _object in sampled_objects]
             batch[0]['caption_tips'] = torch.cat(processed_sampled_objects, dim=0)
             caption_model.module._forward = 'caption'
             prediction = caption_model(batch[0])
             captions = convert_items(detach(prediction), tokenizer)
             
             model.module._forward = 'question'
-            processed_tip = torch.cat([process_tip(_object, region_caption) for _object, region_caption in zip(sampled_objects, captions)], dim=0)
+            processed_tip = torch.cat([process_tip(_object[0], region_caption) for _object, region_caption in zip(sampled_objects, captions)], dim=0)
         
             for j in range(repeat_sample):
                 start_sample = time.time()
@@ -128,7 +143,7 @@ def main():
                         'answer': answer,
                         'prob': prob,
                         'type': types[j]['answer_type'],
-                        'object': convert_object(_object),
+                        'object': convert_object(_object[0]),
                         'region_description': _caption
                     }
                     
